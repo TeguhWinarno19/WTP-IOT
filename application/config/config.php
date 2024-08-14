1bce10296d81c3e75d97085324aa1b2881d653ed
Teguh Winarno <163860212+TeguhWinarno19@users.noreply.github.com>
2024-08-14 13:25:13 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/IOT_WTP/';
+$config['base_url'] = 'https://teguhwinarno19.github.io/WTP-IOT/';
 
 /*
 |--------------------------------------------------------------------------
